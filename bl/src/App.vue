6ea705fd369d6c9c0baeb0c07a4063cdd2ac6c09
fuchenxi <fuchenxi8@163.com>
2021-02-24 21:14:27 +0800

    初始化

--- bl/src/App.vue
+++ bl/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <!-- 路由占位符 -->
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
---
+++ src/pathology/workstation.vue
@@ -0,0 +1,6 @@
+<!--病理工作站-->
+<template>
+  <div class="workstation">
+    <i class="el-icon-s-unfold"></i>
+  </div>
+</template>
